d2d03df4ccbdfeb212b78eb2cd67ab4b9a275ccc
Aditya Chaudhari <100196742+adityacdr@users.noreply.github.com>
2023-05-25 17:58:37 +0530

    Flutter Changes 1
    Flutter Changes 1

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ktalm\\Downloads\\flutter_windows_3.10.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ktalm\\OneDrive\\Desktop\\BITS Pilani - Aditya\\Cross Platform Application Development\\VSCode\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ktalm\\Downloads\\flutter_windows_3.10.1-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\ktalm\\OneDrive\\Desktop\\BITS Pilani - Aditya\\Cross Platform Application Development\\VSCode\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\Users\\ktalm\\Downloads\\flutter_windows_3.10.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ktalm\\OneDrive\\Desktop\\BITS Pilani - Aditya\\Cross Platform Application Development\\VSCode\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ktalm\\OneDrive\\Desktop\\BITS Pilani - Aditya\\Cross Platform Application Development\\VSCode\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\ktalm\\OneDrive\\Desktop\\BITS Pilani - Aditya\\Cross Platform Application Development\\VSCode\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iNGZiMTEyMTRkZDJkZGE2Y2UwMTJkZDk4ZWE0OThlOWU4YjkxMjYyLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ktalm\\OneDrive\\Desktop\\BITS Pilani - Aditya\\Cross Platform Application Development\\VSCode\\flutter_application_1\\.dart_tool\\package_config.json"
+)
